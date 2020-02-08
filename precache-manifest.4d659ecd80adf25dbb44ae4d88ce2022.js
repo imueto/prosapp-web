dfc8a191f83fcc8c8b453740c5d2f80298f56520
Imanol Muelas Toledo <imanol.toledo@eggplant.io>
2020-02-08 18:55:28 +0000

    Website

---
+++ precache-manifest.4d659ecd80adf25dbb44ae4d88ce2022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1148908b162acd830f0be06d198626b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5bbda6639059fc286db",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ee187fc55ed52d3d59ff",
+    "url": "/static/css/main.3205ae5a.chunk.css"
+  },
+  {
+    "revision": "e5bbda6639059fc286db",
+    "url": "/static/js/2.858f37b1.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.858f37b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee187fc55ed52d3d59ff",
+    "url": "/static/js/main.ae980b89.chunk.js"
+  },
+  {
+    "revision": "1a0b23bd377b98e8c367",
+    "url": "/static/js/runtime-main.7fddb1e2.js"
+  }
+]);
